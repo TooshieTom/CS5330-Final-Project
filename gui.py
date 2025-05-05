2d906e9eddf2adf7aec5e9cef166850669a051d4
ElijahChesnut26 <echesnut@mail.smu.edu>
2025-05-05 15:44:33 -0500

    Update GUI
    Can insert and single query

--- gui.py
+++ gui.py
@@ -11,7 +11,7 @@ root = tk.Tk()
 root.geometry("750x800")
 root.title("Databases")
 
-def change_frame(*args):
+def IQ_change_frame(*args):
     # print("hello")
     if IQ_selected.get() == "Insert":
         # clear_frame(qframe)
@@ -81,45 +81,86 @@ def i_submit(*args):
 
 
 def update_columns(*args):
-    if from_var.get() == "Users":
-        # print("Hello")
-        #show usersQ_frame
+    x = from_var.get()
+    if x == "Users":
+        qframe_sub.pack_forget()
+        qframe_post.pack_forget()
+        qframe_project.pack_forget()
+        qframe_record.pack_forget()
+        q_columns_label.pack()
         qframe_users.pack()
-    else:
-        # print("Goodbye")
+        qframe_sub.pack()
+    elif x == "Post":
+        qframe_sub.pack_forget()
         qframe_users.pack_forget()
+        qframe_project.pack_forget()
+        qframe_record.pack_forget()
+        q_columns_label.pack()
+        qframe_post.pack()
+        qframe_sub.pack()
+    elif x == "Project":
+        qframe_sub.pack_forget()
+        qframe_users.pack_forget()
+        qframe_post.pack_forget()
+        qframe_record.pack_forget()
+        q_columns_label.pack()
+        qframe_project.pack()
+        qframe_sub.pack()
+    elif x == "Record":
+        qframe_sub.pack_forget()
+        qframe_users.pack_forget()
+        qframe_post.pack_forget()
+        qframe_project.pack_forget()
+        q_columns_label.pack()
+        qframe_record.pack()
+        qframe_sub.pack()
 
-def do_something(table):
-    # print(q_users_entry.get())
-    # q_users_entry.delete(0, tk.END)
-    entries = [e.get() for e in q_entries]
-    print(table.get())
-    for e in entries:
-        # if isinstance(e, tk.Entry):
-        print(e)
-        # else:
-        #     print(e.get())
+def q_submit(table):
+    # entries = [e.get() for e in q_entries]
+    # print(table.get())
+    # for e in entries:
+    #     print(e)
+    x = table.get()
+    if x == "Users":
+        print(x)
+        print(q_columns_user_var.get())
+        print(q_drop_op_var.get())
+        print(q_entry_value.get())
+    elif x == "Post":
+        print(x)
+        print(q_columns_post_var.get())
+        print(q_drop_op_var.get())
+        print(q_entry_value.get())
+    elif x == "Project":
+        print(x)
+        print(q_columns_project_var.get())
+        print(q_drop_op_var.get())
+        print(q_entry_value.get())
+    elif x == "Record":
+        print(x)
+        print(q_columns_record_var.get())
+        print(q_drop_op_var.get())
+        print(q_entry_value.get())
+        
     
-def show_more(*args):
-    global checked 
-    if not checked:
-        qframe_users_more.pack()
-        checked = not checked
-    else:
-        qframe_users_more.pack_forget()
-        checked = not checked
+# def show_more(*args):
+#     global checked 
+#     if not checked:
+#         qframe_users_more.pack()
+#         checked = not checked
+#     else:
+#         qframe_users_more.pack_forget()
+#         checked = not checked
 
 iframe = tk.Frame(root)
 qframe = tk.Frame(root)
 
 IQ_selected = tk.StringVar(value="Select")
-IQ_selected.trace_add('write', change_frame)
+IQ_selected.trace_add('write', IQ_change_frame)
 drop = tk.OptionMenu(root, IQ_selected, "Insert", "Query")
 drop.pack(pady=10)
 
-# word = tk.StringVar(value="Hello")
-# label = tk.Label(iframe, text="hello")
-# label.pack()
+
 """INSERT FRAME"""
 i_table_label = tk.Label(iframe, text="TABLE")
 i_table_drop_var = tk.StringVar(value="Choose Table")
@@ -175,40 +216,83 @@ for attribute in tables["Record"]:
 
 
 """QUERY FRAME"""
+qframe_sub = tk.Frame(qframe)
+# qframe_sub.pack()
+
+
+#table dropdown
 from_label = tk.Label(qframe, text="FROM")
-from_label.pack()
 from_var = tk.StringVar(qframe, value="Choose")
 from_var.trace_add('write', update_columns)
 from_drop = tk.OptionMenu(qframe, from_var, *tables.keys())
-from_drop.pack()
+
 
 q_entries = []
 
-"""USERS"""
-qframe_users = tk.Frame(qframe)
-qframe_users_more = tk.Frame(qframe_users)
+#operator dropdown
+q_drop_op_label = tk.Label(qframe_sub, text="OPERATOR")
+q_drop_op_var = tk.StringVar(qframe_sub, value="Choose")
+q_drop_op = tk.OptionMenu(qframe_sub, q_drop_op_var, "=", "<", "<=", ">=", ">", "!=")
 
+#value entry
+q_label_value = tk.Label(qframe_sub, text="VALUE")
+q_entry_value = tk.Entry(qframe_sub)
+q_label_socmed = tk.Label(qframe_sub, text="Social Media")
+button = tk.Button(qframe_sub, text="Submit", command=lambda: q_submit(from_var))
 
-q_users_drop_var = tk.StringVar(qframe_users, value="Choose")
-q_users_drop = tk.OptionMenu(qframe_users, q_users_drop_var, "=", "<", "<=", ">=", ">", "!=")
+q_columns_label = tk.Label(qframe, text="COLUMN")
 
-q_users_label_value = tk.Label(qframe_users, text="VALUE")
-q_users_entry_value = tk.Entry(qframe_users)
-q_users_label_socmed = tk.Label(qframe_users, text="Social Media")
-button = tk.Button(qframe_users, text="button", command=lambda: do_something(from_var))
+#qframe packing
+from_label.pack()
+from_drop.pack()
 
-
-q_users_label_value.pack()
-q_users_entry_value.pack()
+#qframe_sub pack
+# q_columns_label.pack()#############
+q_drop_op_label.pack()
+q_drop_op.pack()
+q_label_value.pack()
+q_entry_value.pack()
 button.pack()
 
-"""MORE"""
-checked = False
-check_button = tk.Button(qframe_users, text="more", command=show_more)
-check_button.pack()
+#subframes
+qframe_users = tk.Frame(qframe)
+# qframe_users_more = tk.Frame(qframe_users)############################
+qframe_post = tk.Frame(qframe)
+qframe_project = tk.Frame(qframe)
+qframe_record = tk.Frame(qframe)
 
-l = tk.Label(qframe_users_more, text="MORE")
-l.pack()
+"""USERS"""
+q_columns_user_var = tk.StringVar(qframe_users, value="Choose")
+q_columns_user_drop = tk.OptionMenu(qframe_users, q_columns_user_var, *tables["Users"])
+
+
+q_columns_user_drop.pack()
+
+"""POST"""
+q_columns_post_var = tk.StringVar(qframe_post, value="Choose")
+q_columns_post_drop = tk.OptionMenu(qframe_post, q_columns_post_var, *tables["Post"])
+
+q_columns_post_drop.pack()
+
+"""PROJECT"""
+q_columns_project_var = tk.StringVar(qframe_project, value="Choose")
+q_columns_project_drop = tk.OptionMenu(qframe_project, q_columns_project_var, *tables["Project"])
+
+q_columns_project_drop.pack()
+
+""""RECORD"""
+q_columns_record_var = tk.StringVar(qframe_record, value="Choose")
+q_columns_record_drop = tk.OptionMenu(qframe_record, q_columns_record_var, *tables["Record"])
+
+q_columns_record_drop.pack()
+
+"""MORE"""
+# checked = False
+# check_button = tk.Button(qframe_users, text="more", command=show_more)
+# check_button.pack()
+
+# l = tk.Label(qframe_users_more, text="MORE")
+# l.pack()
 
 
 root.mainloop()
