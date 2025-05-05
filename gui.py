d2add32e4417af14bfb82e01c8eb59d4a0f6bb83
ElijahChesnut26 <echesnut@mail.smu.edu>
2025-05-05 13:44:55 -0500

    GUI overhaul

--- gui.py
+++ gui.py
@@ -1,10 +1,5 @@
 import tkinter as tk
 
-root = tk.Tk()
-root.title("Database GUI")
-root.geometry("800x600")
-
-# Tables
 tables = {
     "Users": ["Username", "Soc_Med", "Name", "Verified", "Country_Birth", "Country_Res", "Age", "Gender"],
     "Post": ["Username", "Soc_Med", "Time_Posted", "City", "State", "Country", "Multimedia", "Likes", "Dislikes", "Text", "Poster_OG", "Time_OG"],
@@ -12,192 +7,208 @@ tables = {
     "Record": ["Project", "Text", "Fields", "Username", "Time_Posted", "Soc_Med"]
 }
 
-# Variables
-mode_var = tk.StringVar(value="Choose action")
-table_var = tk.StringVar(value="Choose table")
+root = tk.Tk()
+root.geometry("750x800")
+root.title("Databases")
 
-entries = []  # To store current entries
+def change_frame(*args):
+    # print("hello")
+    if IQ_selected.get() == "Insert":
+        # clear_frame(qframe)
+        qframe.pack_forget()
+        iframe.pack()
+    else:
+        # clear_frame(iframe)
+        iframe.pack_forget()
+        qframe.pack()
 
-# Frames
-mode_frame = tk.Frame(root)
-mode_frame.pack(pady=10)
+# def clear_frame(frame):
+#     for widgets in frame.winfo_children():
+#         widgets.destroy()
+def i_table_frame(*args):
+    x = i_table_drop_var.get()
+    # print(x)
+    if x == "Users":
+        i_entries.clear()
+        i_button.pack_forget()
+        iframe_post.pack_forget()
+        iframe_project.pack_forget()
+        iframe_record.pack_forget()
+        iframe_users.pack()
+        for widget in iframe_users.winfo_children():
+            if isinstance(widget, tk.Entry):
+                i_entries.append(widget)
+    elif x == "Post":
+        i_entries.clear()
+        i_button.pack_forget()
+        iframe_users.pack_forget()
+        iframe_project.pack_forget()
+        iframe_record.pack_forget()
+        iframe_post.pack()
+        for widget in iframe_post.winfo_children():
+            if isinstance(widget, tk.Entry):
+                i_entries.append(widget)
+    elif x == "Project":
+        i_entries.clear()
+        i_button.pack_forget()
+        iframe_users.pack_forget()
+        iframe_post.pack_forget()
+        iframe_record.pack_forget()
+        iframe_project.pack()
+        for widget in iframe_project.winfo_children():
+            if isinstance(widget, tk.Entry):
+                i_entries.append(widget)
+    elif x == "Record":
+        i_entries.clear()
+        i_button.pack_forget()
+        iframe_users.pack_forget()
+        iframe_post.pack_forget()
+        iframe_project.pack_forget()
+        iframe_record.pack()
+        for widget in iframe_record.winfo_children():
+            if isinstance(widget, tk.Entry):
+                i_entries.append(widget)
+    i_button.pack()
 
-dynamic_frame = tk.Frame(root)
-dynamic_frame.pack(pady=10)
-
-# --- Functions ---
-
-def clear_frame():
-    for widget in dynamic_frame.winfo_children():
-        widget.destroy()
-    entries.clear()
-
-trace_id = None
-
-def add_select_field():
-    """Adds another set of SELECT column, operator, and value entry fields."""
-    # SELECT column dropdown
-    select_label = tk.Label(dynamic_frame, text="SELECT")
-    select_label.pack()
-
-    new_column_var = tk.StringVar()
-    new_column_var.set("Choose column")
-    column_drop = tk.OptionMenu(dynamic_frame, new_column_var, *tables[table_var.get()])
-    column_drop.pack()
-
-    # Operator dropdown
-    operator_label = tk.Label(dynamic_frame, text="Choose Operator")
-    operator_label.pack()
-    new_operator_var = tk.StringVar()
-    new_operator_var.set("=")  # Default to equals operator
-    operator_drop = tk.OptionMenu(dynamic_frame, new_operator_var, '=', '<', '<=', '>', '>=', '!=')
-    operator_drop.pack(pady=5)
-
-    # Value input field
-    value_label = tk.Label(dynamic_frame, text="Enter Value")
-    value_label.pack()
-    new_value_entry = tk.Entry(dynamic_frame)
-    new_value_entry.pack(pady=5)
-
-def update_ui(*args):
-    global trace_id ####
-    clear_frame()
-    selected_mode = mode_var.get()
-
-    if selected_mode == "Insert":
-        table_var.set("Choose table")
-        table_dropdown = tk.OptionMenu(dynamic_frame, table_var, *tables.keys())
-        table_dropdown.pack(pady=5)
-        table_var.trace_add('write', show_table_entries)
-
-    elif selected_mode == "Query": 
-        #clear
-        if trace_id is not None:
-            table_var.trace_remove('write', trace_id)
-
-
-        #select table dropdown
-        from_label = tk.Label(dynamic_frame, text="FROM")
-        from_label.pack()
-        table_var.set("Choose table")
-        select_drop = tk.OptionMenu(dynamic_frame, table_var, *tables.keys())
-        select_drop.pack()
-
-         # Function to update select column options based on the selected table
-        def update_columns(*args):
-            selected_table = table_var.get()
-            # if selected_table != "Choose table":
-            select_label = tk.Label(dynamic_frame, text="SELECT")
-            select_label.pack()
-
-            # Update the columns dropdown to show the columns of the selected table
-            column_var.set("Choose column")
-            column_drop = tk.OptionMenu(dynamic_frame, column_var, *tables[selected_table])
-            column_drop.pack()
-
-            # Operator dropdown
-            operator_label = tk.Label(dynamic_frame, text="Choose Operator")
-            operator_label.pack()
-            operator_var.set("=")  # Default to equals operator
-            operator_drop = tk.OptionMenu(dynamic_frame, operator_var, '=', '<', '<=', '>', '>=', '!=')
-            operator_drop.pack(pady=5)
-
-            # Value input field
-            value_label = tk.Label(dynamic_frame, text="Enter Value")
-            value_label.pack()
-            value_entry = tk.Entry(dynamic_frame)
-            value_entry.pack(pady=5)
+def i_submit(*args):
+    print("SUBMITTED")
+    entries = []
+    for i in i_entries:
+        entries.append(i.get())
+        i.delete(0, tk.END)
+    for e in entries:
+        print(e)
 
 
-            def submit_query():
-                selected_table = table_var.get()
-                selected_column = column_var.get()
-                selected_operator = operator_var.get()
-                entered_value = value_entry.get()
+def update_columns(*args):
+    if from_var.get() == "Users":
+        # print("Hello")
+        #show usersQ_frame
+        qframe_users.pack()
+    else:
+        # print("Goodbye")
+        qframe_users.pack_forget()
 
-                # Print all the inputs to the terminal
-                print(f"Table: {selected_table}")
-                print(f"Column: {selected_column}")
-                print(f"Operator: {selected_operator}")
-                print(f"Value: {entered_value}")
-                clear_frame()
-            # def submit_query():
-            #     """Collects all inputs from dynamically added fields and prints the query."""
-            #     selected_table = table_var.get()
-            #     query_conditions = []
-
-            #     # Iterate through all widgets in the dynamic_frame to collect inputs
-            #     for widget in dynamic_frame.winfo_children():
-            #         if isinstance(widget, tk.OptionMenu):  # Dropdowns
-            #             # Get the associated StringVar for the dropdown
-            #             var = widget.cget("textvariable")
-            #             value = root.nametowidget(var).get()
-            #             query_conditions.append(value)
-            #         elif isinstance(widget, tk.Entry):  # Entry fields
-            #             value = widget.get()
-            #             query_conditions.append(value)
-
-            #     # Print the collected query conditions
-            #     print(f"Table: {selected_table}")
-            #     print("Query Conditions:")
-            #     for i in range(0, len(query_conditions), 3):  # Group by SELECT, Operator, Value
-            #         print(f"Column: {query_conditions[i]}, Operator: {query_conditions[i+1]}, Value: {query_conditions[i+2]}")
-
-            #     clear_frame()
-
-
-            # Add a checkmark button to add more fields
-            add_field_btn = tk.Button(dynamic_frame, text="✔ Add Field", command=add_select_field)
-            add_field_btn.pack(pady=10)
-
-            submit_btn = tk.Button(dynamic_frame, text="Submit Query", command=submit_query)
-            submit_btn.pack(pady=10)
-
-        # Add a trace to the table_var to update the columns dropdown when a table is selected
-        column_var = tk.StringVar()
-        column_var.set("Choose column")
-        operator_var = tk.StringVar()
-        value_var = tk.StringVar()
-        table_var.trace_add('write', update_columns)
-
-        
-def show_table_entries(*args):
-    clear_frame()
+def do_something(table):
+    # print(q_users_entry.get())
+    # q_users_entry.delete(0, tk.END)
+    entries = [e.get() for e in q_entries]
+    print(table.get())
+    for e in entries:
+        # if isinstance(e, tk.Entry):
+        print(e)
+        # else:
+        #     print(e.get())
     
-    table_name = table_var.get()
-    attributes = tables.get(table_name, [])
+def show_more(*args):
+    global checked 
+    if not checked:
+        qframe_users_more.pack()
+        checked = not checked
+    else:
+        qframe_users_more.pack_forget()
+        checked = not checked
 
-    for attr in attributes:
-        label = tk.Label(dynamic_frame, text=attr)
-        label.pack()
-        entry = tk.Entry(dynamic_frame)
-        entry.pack(pady=2)
-        entries.append(entry)
+iframe = tk.Frame(root)
+qframe = tk.Frame(root)
 
-    submit_btn = tk.Button(dynamic_frame, text="Submit Insert", command=submit_insert)
-    submit_btn.pack(pady=10)
+IQ_selected = tk.StringVar(value="Select")
+IQ_selected.trace_add('write', change_frame)
+drop = tk.OptionMenu(root, IQ_selected, "Insert", "Query")
+drop.pack(pady=10)
 
-def submit_insert():
-    values = [e.get() for e in entries]
-    print("Insert into", table_var.get(), "values:", values)
-    #Send info to other function 
-    clear_frame()
+# word = tk.StringVar(value="Hello")
+# label = tk.Label(iframe, text="hello")
+# label.pack()
+"""INSERT FRAME"""
+i_table_label = tk.Label(iframe, text="TABLE")
+i_table_drop_var = tk.StringVar(value="Choose Table")
+i_table_drop_var.trace_add('write', i_table_frame)
+i_table_drop = tk.OptionMenu(iframe, i_table_drop_var, *tables.keys())
 
-# def submit_query():
-#     parts = [e.get() for e in entries]
-#     print("Query:", f"SELECT {parts[0]} FROM {parts[1]} WHERE {parts[2]}")
-#     #Send info to other function
+i_button = tk.Button(iframe, text="Submit Info" ,command=i_submit)
+
+i_entries = []
+
+i_table_label.pack()
+i_table_drop.pack()
+
+"""IFRAME USERS"""
+iframe_users = tk.Frame(iframe)
+
+for attribute in tables["Users"]:
+    i_users_label = tk.Label(iframe_users, text=attribute)
+    i_users_entry = tk.Entry(iframe_users)
+    i_users_label.pack()
+    i_users_entry.pack()
+    # i_entries.append(i_users_entry)
+
+"""IFRAME POST"""
+iframe_post = tk.Frame(iframe)
+
+for attribute in tables["Post"]:
+    i_post_label = tk.Label(iframe_post, text=attribute)
+    i_post_entry = tk.Entry(iframe_post)
+    i_post_label.pack()
+    i_post_entry.pack()
+    # i_entries.append(i_post_entry)
+
+"""IFRAME PROJECT"""
+iframe_project = tk.Frame(iframe)
+
+for attribute in tables["Project"]:
+    i_project_label = tk.Label(iframe_project, text=attribute)
+    i_project_entry = tk.Entry(iframe_project)
+    i_project_label.pack()
+    i_project_entry.pack()
+    # i_entries.append(i_project_entry)
+
+"""IFRAME RECORD"""
+iframe_record = tk.Frame(iframe)
+
+for attribute in tables["Record"]:
+    i_record_label = tk.Label(iframe_record, text=attribute)
+    i_record_entry = tk.Entry(iframe_record)
+    i_record_label.pack()
+    i_record_entry.pack()
+    # i_entries.append(i_record_entry)
 
 
+"""QUERY FRAME"""
+from_label = tk.Label(qframe, text="FROM")
+from_label.pack()
+from_var = tk.StringVar(qframe, value="Choose")
+from_var.trace_add('write', update_columns)
+from_drop = tk.OptionMenu(qframe, from_var, *tables.keys())
+from_drop.pack()
 
-# --- Dropdown to choose Insert or Query ---
+q_entries = []
 
-mode_dropdown = tk.OptionMenu(mode_frame, mode_var, "Insert", "Query")
-mode_dropdown.pack()
+"""USERS"""
+qframe_users = tk.Frame(qframe)
+qframe_users_more = tk.Frame(qframe_users)
 
-mode_var.trace_add('write', update_ui)
 
-# print(tables["Users"])
-# print(*tables.keys())
+q_users_drop_var = tk.StringVar(qframe_users, value="Choose")
+q_users_drop = tk.OptionMenu(qframe_users, q_users_drop_var, "=", "<", "<=", ">=", ">", "!=")
+
+q_users_label_value = tk.Label(qframe_users, text="VALUE")
+q_users_entry_value = tk.Entry(qframe_users)
+q_users_label_socmed = tk.Label(qframe_users, text="Social Media")
+button = tk.Button(qframe_users, text="button", command=lambda: do_something(from_var))
+
+
+q_users_label_value.pack()
+q_users_entry_value.pack()
+button.pack()
+
+"""MORE"""
+checked = False
+check_button = tk.Button(qframe_users, text="more", command=show_more)
+check_button.pack()
+
+l = tk.Label(qframe_users_more, text="MORE")
+l.pack()
+
 
 root.mainloop()
